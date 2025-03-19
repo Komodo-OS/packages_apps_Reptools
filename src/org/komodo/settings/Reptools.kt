b5c275b8c11d2b7085c6a0ec89674f2ea1a3dfa1
Kry9toN <kry9ton@kryptonproject.my.id>
2025-03-19 09:23:09 +0000

    initial Reptools base on LOS

---
+++ src/org/komodo/settings/Reptools.kt
@@ -0,0 +1,30 @@
+package org.komodo.settings
+
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.dashboard.DashboardFragment
+import com.android.settings.R
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.search.SearchIndexable
+
+@SearchIndexable
+class Reptools : DashboardFragment() {
+
+    override fun getPreferenceScreenResId(): Int {
+        return R.xml.reptools_dashboard
+    }
+
+    override fun getMetricsCategory(): Int {
+        return MetricsProto.MetricsEvent.KOMODO
+    }
+
+    override fun getLogTag(): String {
+        return TAG
+    }
+
+    companion object {
+        private const val TAG = "Reptools"
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = BaseSearchIndexProvider(R.xml.reptools_dashboard)
+    }
+}
